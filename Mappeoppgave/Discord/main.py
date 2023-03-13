aa69eafb46c50b9912576b72a9520acd3c7df1fa
Danieljoha <djo060@uit.no>
2023-03-13 13:57:28 +0100

    Arbeidskrav_6 ferdig

--- Mappeoppgave/Discord/main.py
+++ Mappeoppgave/Discord/main.py
@@ -4,24 +4,30 @@ import discord
 from discord.ext import commands
 import requests
 from bs4 import BeautifulSoup
-
-
+import asyncio
 
 # Load environment variables from .env file
-load_dotenv(dotenv_path=r'C:\Users\Daniel\Documents\Rstudio GIT\Sok-1006\Sok-1006\Mappeoppgave\Discord\secret_bot.env')
+load_dotenv(dotenv_path=r'Mappeoppgave\Discord\secret_bot.env')
 
 # Read the Discord bot token from environment variable
 DISCORD_BOT_TOKEN = os.getenv('secret_bot')
 
-
-
 # Create a new Discord bot instance
 intents = discord.Intents.all()
 intents.members = True
-bot = commands.Bot(command_prefix='', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Define a function to scrape the website and check for rows with specific keywords
-def check_course_plan():
+last_match = None  # Initialize last_match to None to indicate that no matching rows have been found yet
+
+async def post_new_row(row):
+    # Define a function to post the new row to Discord
+    channel = bot.get_channel(1065637170708222052)  # ID of the Discord channel where it posts
+    await channel.send(f"God dag god dag studentan \nNy oppgave i Sok-1005:\n{row}")
+
+async def check_course_plan():
+    global last_match  # Access the global last_match variable
+
     url = 'https://uit-sok-1005-v23.github.io/courseplan.html'
 
     # make a GET request to the website and get the HTML content
@@ -48,20 +54,42 @@ def check_course_plan():
         rows.append(row)
 
     # check each row for the words "task", "assignment", and "deadline"
+    latest_match = None
     for row in rows:
         if any("task" in value.lower() or "assignment" in value.lower() or "deadline" in value.lower() for value in row.values()):
-            return f"Session: {row['Session']}\nTopics/Resources: {row['Topics/Resources']}"
+            latest_match = f"{row['Date']} {row['Session']} {row['Topics/Resources']}"
+
+    if latest_match and latest_match != last_match:
+        # If a new matching row has been found, and it is different from the last known matching row,
+        # then assume that a new row has been added and return it
+        last_match = latest_match  # Update the last_match variable to the latest matching row
+        await post_new_row(latest_match) # Post the new row to Discord
+        return latest_match
+    
+    # If no new matching row has been found, return None
+    return None
+
+async def periodic_check():
+    while True:
+        await check_course_plan() # Wait for the result of check_course_plan()
+        await asyncio.sleep(60 * 5)  # Wait for 5 minutes before checking again
+
+# Start the periodic check loop asynchronously
+async def start_periodic_check():
+    asyncio.create_task(periodic_check())
+
 
 # Define a command to check the course plan and post the result to Discord
 @bot.command(name='check')
 async def check_command(ctx):
     print('Received check command from', ctx.author.name)
-    result = check_course_plan()
+    result = await check_course_plan()  # Await the result of check_course_plan()
     if result:
-        await ctx.send(f"A row was added that contains 'task', 'assignment', or 'deadline'.\n{result}")
+        await ctx.send(f"God dag god dag studentan \nNy oppgave i Sok-1005:\n{result}")
     else:
-        await ctx.send("No rows were added that contain 'task', 'assignment', or 'deadline'.")
-
+        # Check if the last_match variable has been set to a non-None value
+        if last_match is not None:
+            await ctx.send("Ikke noe nytt arbeid.")
 
 
 # Run the Discord bot using the token from the environment variable
